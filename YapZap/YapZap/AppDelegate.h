c06e934aa1dcf9458be8ec7c4a130876d8949f6b
Jason Boggess <jasonboggess@gmail.com>
2014-02-04 22:04:01 -0500

    added new project with example controllers

---
+++ YapZap/YapZap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YapZap
+//
+//  Created by Jason R Boggess on 2/4/14.
+//  Copyright (c) 2014 i8code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YapZap/YapZap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YapZap
+//
+//  Created by Jason R Boggess on 2/4/14.
+//  Copyright (c) 2014 i8code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
